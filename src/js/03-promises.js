3b4480f8939b2fd0202f3d7cb94badcb6240d98e
Lyutink <krivenko.lyuda@gmail.com>
2021-11-05 01:21:20 +0300

    add Notiflix in 09-3.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const formRef = document.querySelector('.form');
 const btnCreatePromisesRef = document.querySelector('.form > button');
 
@@ -7,9 +9,9 @@ const btnCreatePromisesRef = document.querySelector('.form > button');
 //console.log(inputDelayRef);
 
 //let delay = 0;
-let firstDelay = 0;
-let step = 0;
-let amount = 0;
+// let firstDelay = 0;
+// let step = 0;
+// let amount = 0;
 
 btnCreatePromisesRef.addEventListener('click', acceptData);
 
@@ -19,14 +21,16 @@ function acceptData(event) {
   // formData.forEach((value, name) => {
     
   //   console.log(value, name)
-  firstDelay = Number((formRef['delay'].value));
-  //delay = firstDelay;
-  step = Number((formRef['step'].value));
-  amount = Number((formRef['amount'].value));
-  console.log(firstDelay);
-  console.log(step);
-  console.log(amount);
-  createPromise(firstDelay, step, amount);
+  const dataForPromis = {
+    firstDelay: Number((formRef['delay'].value)),
+   step: Number((formRef['step'].value)),
+    amount: Number((formRef['amount'].value)),
+  }
+
+  console.log(dataForPromis.firstDelay);
+  console.log(dataForPromis.step);
+  console.log(dataForPromis.amount);
+  createPromise(dataForPromis);
 }
 
 
@@ -39,17 +43,22 @@ function acceptData(event) {
 //   }
 // }
 
-function createPromise(firstDelay, step, amount) {
+function createPromise({ firstDelay, step, amount }) {
   let delay = firstDelay;
   console.log('hhhhh',delay);
   for (let i = 1; i <= amount; i += 1) {
   new Promise(resolve => {
     setTimeout(() => resolve(i), delay);
-  }).then(value => console.log(`Fulfilled promise ${value} in ${delay}`));
+  }).then((value) => Notiflix.Notify.success(`Fulfilled promise ${value} in ${delay - (amount - value) * step}`));
     delay += step;
     console.log('hhhhh',delay);
 }
 }
+
+// function makePromisStyle() {
+//   const makeString = '<div>Fulfilled promise</div>';
+//   makeString.insertAdjacentHTML("afterend", formRef);
+// }
 // const firstDelay = 1000;
 // let delay = firstDelay;
 // for (let i = 0; i < 3; i += 1) {
