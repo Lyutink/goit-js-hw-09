927b0df6f919ea2415714037eaec8c07819f2180
Lyutink <krivenko.lyuda@gmail.com>
2021-11-05 02:14:59 +0300

    up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -30,7 +30,7 @@ function acceptData(event) {
   console.log(dataForPromis.firstDelay);
   console.log(dataForPromis.step);
   console.log(dataForPromis.amount);
-  createPromise(dataForPromis);
+  foo(dataForPromis);
 }
 
 
@@ -42,18 +42,45 @@ function acceptData(event) {
 //     // Reject
 //   }
 // }
-
-function createPromise({ firstDelay, step, amount }) {
+function foo({ firstDelay, step, amount }) {
   let delay = firstDelay;
-  console.log('hhhhh',delay);
-  for (let i = 1; i <= amount; i += 1) {
-  new Promise(resolve => {
-    setTimeout(() => resolve(i), delay);
-  }).then((value) => Notiflix.Notify.success(`Fulfilled promise ${value} in ${delay - (amount - value) * step}`));
+
+  for (let position = 1; position <= amount; position += 1){
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({ position, delay });
+          reject({ position, delay });
+        }, delay);
+        
+        
+      })
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}`)
+        })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`)
+      });
+    //createPromise(position, delay);
     delay += step;
-    console.log('hhhhh',delay);
+  }
 }
-}
+
+
+
+
+
+// function createPromise({ firstDelay, step, amount }) {
+//   let delay = firstDelay;
+//   console.log('hhhhh',delay);
+//   for (let i = 1; i <= amount; i += 1) {
+//   new Promise(resolve => {
+//     setTimeout(() => resolve(i), delay);
+//   }).then((value) => Notiflix.Notify.success(`Fulfilled promise ${value} in ${delay - (amount - value) * step}`))
+//     //.catch(({i, delay}))
+//     delay += step;
+//     console.log('hhhhh',delay);
+// }
+// }
 
 // function makePromisStyle() {
 //   const makeString = '<div>Fulfilled promise</div>';
