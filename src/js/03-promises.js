3b903c3720ebda80b2600c1a3fc9e7f8bb18eac8
Lyutink <krivenko.lyuda@gmail.com>
2021-11-04 23:24:50 +0300

    up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,32 @@
+const formRef = document.querySelector('.form');
+const btnCreatePromisesRef = document.querySelector('.form > button');
+
+
+
+//console.log(formRef.step);
+//console.log(inputDelayRef);
+
+let delay = 0;
+let step = 0;
+let amount = 0;
+
+btnCreatePromisesRef.addEventListener('click', acceptData);
+
+function acceptData(event) {
+  event.preventDefault();
+  // const formData = new FormData(formRef);
+  // formData.forEach((value, name) => {
+    
+  //   console.log(value, name)
+  delay = (formRef['delay'].value);
+  step = (formRef['step'].value);
+  amount = (formRef['amount'].value);
+  console.log(delay);
+  console.log(step);
+  console.log(amount);
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
